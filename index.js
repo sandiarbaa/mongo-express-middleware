00b69d828098854c22ba6bc56e46ec1fb020f89f
sandiarbaa <sandiarbaa@gmail.com>
2024-08-27 14:49:33 +0700

    contoh logic middleware authentication

--- index.js
+++ index.js
@@ -3,18 +3,20 @@ const app = express();
 morgan = require("morgan");
 
 app.use(morgan("dev"));
-
 app.use((req, res, next) => {
-  //req.method = "PUT"; // meng-overwrite method saja bisa apa lagi membuat properti baru
-  //req.timeRequest = Date.now(); // secara default properti timeRequest tidak ada, ini menambahkan sendiri untuk mencoba saja kalao di middleware itu bisa mengotak atik object req dan res
   // console.log(req.method, req.url);
   next();
 });
 
-// di dalam app.use kita juga dapat menambahkan suatu path sama seperti route
-// app.use("/hello", (req, res, next) => {
-//   res.send("hello");
-// });
+// gambaran penggunaan middleware untuk authentication
+// tapi middleware ini masih diterapkan ke semua route
+app.use((req, res, next) => {
+  const { password } = req.query;
+  if (password === "admin") {
+    next();
+  }
+  res.send("Perlu masukkan password!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World");
@@ -25,16 +27,13 @@ app.get("/halaman", (req, res) => {
   res.send("halaman");
 });
 
-// app.use bisa digunakan juga di paling bawah sini, bukan hanya di atas
-// karena dia di paling bawah, kita tidak perlu menambahkan next
+app.get("/admin", (req, res) => {
+  res.send("Hello admin");
+});
+
 app.use((req, res) => {
-  // res.send("Page not found!");
   res.status(404).send("Page not found!");
 });
-// sama fungsinya dengan ini, cuman kalau ini kan menggunakan route, masih bisa didahulukan middleware
-// app.get("/*", (req, res) => {
-//   res.send("Page not found!");
-// });
 
 app.listen(3000, () => {
   console.log("Server is running on port http://localhost:3000");
