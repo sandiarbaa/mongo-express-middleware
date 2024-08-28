e4cf8d0981e435c411267b5da6ae09dc9fa473ac
sandiarbaa <sandiarbaa@gmail.com>
2024-08-29 06:36:56 +0700

    memahami error handling #1

--- index.js
+++ index.js
@@ -3,31 +3,14 @@ const app = express();
 morgan = require("morgan");
 
 app.use(morgan("dev"));
-// app.use((req, res, next) => {
-//   // console.log(req.method, req.url);
-//   console.log("1");
-//   next();
-// });
-
-// gambaran penggunaan middleware untuk authentication
-// tapi middleware ini masih diterapkan ke semua route
-// app.use((req, res, next) => {
-//   const { password } = req.query;
-//   if (password === "admin") {
-//     next();
-//   }
-//   res.send("Perlu masukkan password!");
-// });
-
-// itu diatas diubah menjadi function dan dipanggil di route yg spesifik ingin menggunakan middleware ini
-// ini function yg berisikan middleware
 const auth = (req, res, next) => {
   const { password } = req.query;
   if (password === "admin") {
     console.log("2");
     next();
   }
-  res.send("Perlu masukkan password!");
+  // res.send("Perlu masukkan password!");
+  throw new Error("Perlu masukkan password");
 };
 
 app.get("/", (req, res) => {
@@ -39,34 +22,13 @@ app.get("/halaman", (req, res) => {
   res.send("halaman");
 });
 
-// kalau sudah gini, sebelum mengeksekusi respon atau apa yg ada di parameter ke3 dari get nya, akan di cek dulu middleware yg dipasangnya
-// apakah http request nya lolos filter atau tidak di middleware nya dalam contoh ini middleware auth
-// kalau berhasil maka akan melewati next yg ad di middleware itu, dan akan menjalankan parameter ke 3
+app.get("/error", (req, res) => {
+  bird.fly;
+});
+
 app.get("/admin", auth, (req, res) => {
   res.send("Hello admin");
 });
-
-// middleware bisa juga di tuliskan langsung di parameter route nya di get ini
-// tapi kaya gini ga cantik sintaksnya, bagusnya sih di pisah
-// app.get(
-//   "/admin",
-//   (req, res, next) => {
-//     const { password } = req.query;
-//     if (password === "admin") {
-//       console.log("2");
-//       next();
-//     }
-//     res.send("Perlu masukkan password!");
-//   },
-//   (req, res) => {
-//     res.send("Hello admin");
-//   }
-// );
-
-// di app.get ini setelah path itu bisa mengisikan banyak callback
-// dan callbacknya itu bisa digunakan untuk memanggil middleware atau dibuat sebagai middleware
-// dan dapat middlewarenya ini bisa lebih dari 1 tinggal di pisah aja pake koma ,
-// atau bisa berisikan kumpulan array yg berisikan middleware
 
 app.use((req, res) => {
   res.status(404).send("Page not found!");
