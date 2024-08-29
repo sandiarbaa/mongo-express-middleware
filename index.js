601a81201b24bfd39e29829ec94d6b7b48eedfd0
sandiarbaa <sandiarbaa@gmail.com>
2024-08-29 07:39:55 +0700

    membuat function atau middleware error handler sendiri #2

--- index.js
+++ index.js
@@ -30,6 +30,17 @@ app.get("/admin", auth, (req, res) => {
   res.send("Hello admin");
 });
 
+// middleware sebagai error handling, ditandai dengan adanya parameter err di parameter ke 1
+// kalau nanti dari route ada yg error pokoknya akan menampilkan middleware ini
+app.use((err, req, res, next) => {
+  console.log("***************");
+  console.log("*****ERROR*****");
+  console.log("***************");
+  // kalau kita buat error dengan throw new error, maka error itu akan masuk ke object err yg di error-handling message nya
+  // console.log(err.message);
+  next(err); // kalau next nya ga ada error di anggep middleware biasa
+}); // tapi kalau ditambahkan maka dia akan mencari middleware atau respons yg menghasilkan atau mengirimkan object throw new error
+
 app.use((req, res) => {
   res.status(404).send("Page not found!");
 });
